64a3d46f91573599056ab35b26e10f68a77df07a
IT25102567 <IT25102567@my.sliit.lk>
2026-05-16 16:45:09 +0530

    review part files

---
+++ src/main/java/com/entity/VerifiedReview.java
@@ -0,0 +1,27 @@
+package com.example.OnlineMedicalStore.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@DiscriminatorValue("VERIFIED")
+@Getter @Setter @NoArgsConstructor
+public class VerifiedReview extends Review {
+
+    @Column(name = "order_id_ref")
+    private Long orderIdRef;
+
+    @Override
+    public String getReviewType() {
+        return "Verified";
+    }
+
+    @Override
+    public String getDisplayBadge() {
+        return "Verified Purchase";
+    }
+}
