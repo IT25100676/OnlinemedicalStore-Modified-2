d5236c99b9ecfbd021b6c16e8270f9e3c74c442e
IT25102567 <IT25102567@my.sliit.lk>
2026-05-16 17:20:12 +0530

    UpdateMediStoreApplication

--- src/main/java/com/controller/ReviewController.java
+++ src/main/java/com/controller/ReviewController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import java.util.List;
 import java.util.Optional;
-
+  
 @Controller
 @RequestMapping("/reviews")
 @RequiredArgsConstructor
