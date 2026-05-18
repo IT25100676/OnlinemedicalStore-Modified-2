b2282b2899f37facda79a33325fa1200d4132932
IT25101745 <IT25101745@my.sliit.lk>
2026-05-19 00:33:24 +0530

    Fix import formatting in PaymentRepository

--- src/main/java/com/repository/PaymentRepository.java
+++ src/main/java/com/repository/PaymentRepository.java
@@ -1,6 +1,6 @@
 package com.example.OnlineMedicalStore.repository;
 
-import com.example.OnlineMedicalStore.entity.Payment;
+import com.example.OnlineMedicalStore.entity.Payment; 
 import com.example.OnlineMedicalStore.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
