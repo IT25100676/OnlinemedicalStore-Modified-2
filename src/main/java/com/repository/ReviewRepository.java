f92539cbaba2b380d886ce744c904d047cf5abcc
IT25102567 <IT25102567@my.sliit.lk>
2026-05-18 11:05:19 +0530

    Add User import to ReviewRepository

--- src/main/java/com/repository/ReviewRepository.java
+++ src/main/java/com/repository/ReviewRepository.java
@@ -2,6 +2,7 @@ package com.example.OnlineMedicalStore.repository;
 
 import com.example.OnlineMedicalStore.entity.Review;
 import com.example.OnlineMedicalStore.entity.Medicine;
+import com.example.OnlineMedicalStore.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
